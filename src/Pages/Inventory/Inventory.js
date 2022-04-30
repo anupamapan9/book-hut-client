c4ec29731f250558be6393c2e5ace81a7852978e
anupamapan9 <anupamapan0905@gmail.com>
2022-05-01 03:26:16 +0600

    update data

--- src/Pages/Inventory/Inventory.js
+++ src/Pages/Inventory/Inventory.js
@@ -1,23 +1,72 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
-import { Link, useParams } from 'react-router-dom';
+import toast from 'react-hot-toast';
+import { useParams } from 'react-router-dom';
 
 const Inventory = () => {
     const { id } = useParams();
     const [book, setBook] = useState({})
+    const { img, email, description, name, price, quantity, supplier } = book;
+    const [bookQuantity, setBookQuantity] = useState(quantity)
     const url = `http://localhost:5000/inventory/${id}`
     useEffect(() => {
         axios.get(url)
             .then(res => {
                 setBook(res.data)
-            }).then(err => {
-                console.log(err)
             })
-    }, [])
-    const { _id, img, email, description, name, price, quantity, supplier } = book;
+    }, [bookQuantity])
+
+
+
+    //  data 
+    const handelUpdateStock = e => {
+        e.preventDefault();
+        const supplierInput = e.target.quantity.value;
+        const newQuantityTotal = parseInt(quantity) + parseInt(supplierInput)
+        const newQuantity = { newQuantityTotal }
+        const putUrl = `http://localhost:5000/update/${id}`
+        fetch(putUrl, {
+            method: "PUT",
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(newQuantity)
+        }).then(res => res.json())
+            .then(data => {
+                if (data.acknowledged === true) {
+                    setBookQuantity(newQuantityTotal);
+                    toast.success("SuccessFully Added")
+                    e.target.reset()
+                }
+            })
+    }
+
+    // deliver a book ---------------------------
+
+    const deliveredBook = () => {
+        const newQuantityTotal = parseInt(quantity) - 1
+        const newQuantity = { newQuantityTotal }
+        const putUrl = `http://localhost:5000/update/${id}`
+        fetch(putUrl, {
+            method: "PUT",
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(newQuantity)
+        }).then(res => res.json())
+            .then(data => {
+                if (data.acknowledged === true) {
+                    setBookQuantity(newQuantityTotal);
+                    toast.success("Product Delivered !!")
+                }
+            })
+    }
+
+
+
     return (
         <div className='flex flex-col md:flex-row justify-around my-5'>
-            <div className='flex justify-center '>
+            <div className='flex justify-center'>
                 <div className="max-w-sm bg-white border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
                     <div className='flex justify-center h-[400px] w-full'>
                         <img className='w-full' src={img} alt="" />
@@ -42,9 +91,9 @@ const Inventory = () => {
             <div>
                 <h2 className='text-center text-[#00307E] font-bold text-2xl mb-5'>You can update your book Quantity here</h2>
                 <div className='text-center'>
-                    <button className='px-5 py-2 border-2 border-[#00307E] hover:bg-[#00307E] hover:text-white duration-200'>Delivered</button>
+                    <button onClick={deliveredBook} className='px-5 py-2 border-2 border-[#00307E] hover:bg-[#00307E] hover:text-white duration-200'>Delivered</button>
                     <p className='text-[#F70000]'>*It will decrease your stock by one</p>
-                    <form className='my-10'>
+                    <form onSubmit={handelUpdateStock} className='my-10'>
                         <h1>Update your Quantity</h1>
                         <input type="number" name="quantity" id="quantity" required /> <br />
                         <input className='px-3 cursor-pointer mt-3 py-2 border-2 border-[#00307E] hover:bg-[#00307E] hover:text-white duration-200' type="submit" value="Update Stock" />
